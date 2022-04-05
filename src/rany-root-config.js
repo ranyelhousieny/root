c1969cca19b4ec21dc50b23d94182945c13c770e
Rany ElHousieny <elhousie@gmail.com>
2022-04-05 10:23:38 -0700

    passing props

--- src/rany-root-config.js
+++ src/rany-root-config.js
@@ -1,20 +1,41 @@
-import { registerApplication, start } from "single-spa";
+import {
+  registerApplication,
+  start,
+} from 'single-spa';
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
+} from 'single-spa-layout';
+import microfrontendLayout from './microfrontend-layout.html';
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
+const data = {
+  props: {
+    myProp: 'Rany',
   },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+};
+const routes =
+  constructRoutes(
+    microfrontendLayout,
+    data
+  );
+const applications =
+  constructApplications({
+    routes,
+    loadApp({ name }) {
+      return System.import(
+        name
+      );
+    },
+  });
+const layoutEngine =
+  constructLayoutEngine({
+    routes,
+    applications,
+  });
 
-applications.forEach(registerApplication);
+applications.forEach(
+  registerApplication
+);
 layoutEngine.activate();
 start();
